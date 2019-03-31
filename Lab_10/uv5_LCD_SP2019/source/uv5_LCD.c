ae95e374afd94a66d0be1c971dd7b91b4acdc303
Jacob Hillebrand <jhillebrand25@gmail.com>
2019-03-31 17:41:27 -0500

    I forgot about my script

---
+++ Lab_10/uv5_LCD_SP2019/source/uv5_LCD.c
@@ -0,0 +1,42 @@
+/*----------------------------------------------------------------------------
+This project code demonstrates how to interface a ARM Cortex M0+ processor with
+a LCD driver IC controller
+*----------------------------------------------------------------------------*/
+#include <MKL25Z4.H>
+#include "gpio_defs.h"
+#include "LCD_4bit.h"
+
+/* create a delay function */
+void Delay(volatile unsigned int time_del) {
+	while (time_del--) {
+		;
+	}
+}
+
+/*------------------------------------------------------------------------------
+	Example for LCD interface
+	*------------------------------------------------------------------------------*/
+void LCD_Example(void) {
+	
+	Init_LCD();				//Call initial setup function
+	Clear_LCD();			//Ensure LCD is clear before beginning
+	
+	//use a function call to initialize the LCD 
+	// use a function call to Clear_LCD();
+	Set_Cursor(0,0);
+	Print_LCD("This is a test"); //Must have 16 letter space
+	//Print_LCD(" Hello, World!:)"); //Must have 16 letter space
+	Set_Cursor(0,1);
+	Print_LCD("for Jacob :/"); //Must have 16 letter space
+}
+
+/*----------------------------------------------------------------------------
+  MAIN function
+ *----------------------------------------------------------------------------*/
+int main (void) {
+			
+		//Run example code
+		LCD_Example();
+
+	
+}
